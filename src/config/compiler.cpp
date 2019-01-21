a5efe51c7e2d69fbc8e1b2ee6f52c3ead9b20e5b
INJAE <8687lee@gmail.com>
2019-01-21 12:18:38 +0900

    update cppm

--- src/config/compiler.cpp
+++ src/config/compiler.cpp
@@ -23,13 +23,11 @@ namespace cppm
     std::string Compiler::generate() {
         using namespace fmt::literals;
         return "if(\"${CMAKE_CXX_COMPILER_ID}\" STREQUAL \"Clang\")\n"
-             + "    set(CMAKE_CXX_FLAGS \"${{CMAKE_CXX_FLAGS}} {0}\") \n"_format(list["clang++"].option)
+             + "    set(CMAKE_CXX_FLAGS \"${{CMAKE_CXX_FLAGS}} {0}\") \n"_format(list["clang"].option)
              + "elseif(\"${CMAKE_CXX_COMPILER_ID}\" STREQUAL \"GNU\")  \n"
-             + "    set(CMAKE_CXX_FLAGS \"${{CMAKE_CXX_FLAGS}} {0}\") \n"_format(list["g++"].option)
-             // + \"elseif (\"${{CMAKE_CXX_COMPILER_ID}}\" STREQUAL \"Intel\")\n\"
-             // + \"set(CMAKE_CXX_FLAGS \"${{CMAKE_CXX_FLAGS}} {0}\")\"_format();
-             // + \"elseif (\"${{CMAKE_CXX_COMPILER_ID}}\"\" STREQUAL \"MSVC\") \n\"
-             // + \"set(CMAKE_CXX_FLAGS \"${{CMAKE_CXX_FLAGS}} {0}\")\"_format();
+             + "    set(CMAKE_CXX_FLAGS \"${{CMAKE_CXX_FLAGS}} {0}\") \n"_format(list["gcc"].option)
+             + "elseif (\"${CMAKE_CXX_COMPILER_ID}\" STREQUAL \"MSVC\") \n"
+             + "    set(CMAKE_CXX_FLAGS \"${{CMAKE_CXX_FLAGS}} {0}\")\n"_format(list["msvc"].option)
              + "endif()\n";
     }
 }
