59c366efa38b93619142ceb1257b601f10a71e9b
injae <8687lee@gmail.com>
2019-04-05 00:30:13 +0900

    update cppm_tools
    now cppkg dependency load more fast

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -33,29 +33,19 @@ namespace cppm
         using namespace fmt::literals;
         return "cmake_minimum_required(VERSION {0})\n"_format(cmake.version)
              + "\n"
+             + "list(APPEND CMAKE_MODULE_PATH \"${CMAKE_CURRENT_SOURCE_DIR}/cmake/\")\n"
              + hunter.generate()
              + "project({0} LANGUAGES CXX VERSION {1})\n"_format(package.name, package.version)
              + "\n"
              + compiler.generate()
-             + "include(cmake/cppm_tool.cmake)\n"
-             + "cppm_setting()\n"
-            //+ "set(CMAKE_CXX_STANDARD {})\n"_format(package.cpp_version)
-            //+ "set(CMAKE_CXX_STANDARD_REQUIRED ON)\n"
-            //+ "set(CMAKE_CXX_EXTENSIONS OFF)\n"
-             + "# detect host processor\n"
-             + "list(APPEND CMAKE_MODULE_PATH \"${CMAKE_CURRENT_SOURCE_DIR}/cmake/Modules/\")\n"
-            //+ "list(APPEND CMAKE_PREFIX_PATH \"$ENV{HOME}/.cppm/local\")"
-             + "\n"
+             + "include(cppm_tool)\n"
+             + "cppm_load()\n"
              + "\n"
              + cmake.generate()
              + dependencies.gen_find_cppkg()
              + tool::target_define(*this)
              + tool::target_dependencies(*this)
              + tool::target_install(*this)
-            //+ "\n"
-            //+ libs.generate(*this)
-            //+ "\n"
-            //+ bins.generate(*this)
              + "\n"
              ;
     }
